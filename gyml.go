203dc68daee1f112d0402429341e4bed3a8e77fa
Matus Ujhelyi <matus.ujhelyi@tind.sk>
2026-02-04 22:08:22 +0100

    feat: add SetValue method first impl

--- gyml.go
+++ gyml.go
@@ -19,7 +19,31 @@ var (
 	ErrInvalidIndexFormat = errors.New("invalid index format")
 	ErrIndexOutOfBound    = errors.New("provided index out of bound")
 	ErrInvalidKeysList    = errors.New("invalid keys list")
+	ErrScalarSetAttempt   = errors.New("cannot iterate over scalar node")
 )
+
+// Returns error on failure
+// Examples:
+// SetValue is used to set values on key's path, if some part of the path does not exist, it is created with correct type based on the keys ([] -> sequence type, "name" -> map type)
+// node - root of yaml
+// data - data to be inserted, can be any type that is serializable into yaml (ScalarType, MapType, SeqType)
+// keys - path in yaml as a list of keys that define place of the the value in yaml
+// Examples:
+// SetValue(&root, TestSomeStruct{Name: "Adam", Age: 30}, "Company", "CEO") - set TestSomeStruct on /Company/CEO
+// SetValue(&root, 35, "some_list", "[]") - append new item 35 to some_list sequence
+// SetValue(&root, 12, "some_list", "[8]") - set 12 in some_list at index[8] (range check involved)
+// SetValue(&root, "Matus", "Company", "CEO", "Name") - scalar value settings at /Company/CEO/Name to Matus
+func SetValue[DataType any](root *yaml.Node, data DataType, keys ...string) error {
+	if len(keys) == 0 {
+		return ErrInvalidKeysList
+	}
+
+	if root == nil {
+		return ErrRootNodeNotSet
+	}
+
+	return setValue(root, data, keys...)
+}
 
 func DeleteValue(root *yaml.Node, keys ...string) error {
 
@@ -49,7 +73,7 @@ func GetValue[DataType any](rootNode *yaml.Node, keys ...string) (*DataType, err
 
 	var value DataType
 	if err := node.Decode(&value); err != nil {
-		return nil, fmt.Errorf("GetValue: cannot unmarshall yaml node value: %w", err)
+		return nil, fmt.Errorf("GetValue: cannot decode yaml node value: %w", err)
 	}
 	normalizeEmptySlice(&value)
 	return &value, nil
@@ -76,6 +100,22 @@ func parseValidIndex(indexStr string, node *yaml.Node) (int, error) {
 	}
 
 	return index, nil
+}
+
+func setValue[DataType any](root *yaml.Node, data DataType, keys ...string) error {
+	if root.Kind == yaml.ScalarNode {
+		return fmt.Errorf("%w: %s", ErrScalarSetAttempt, keys[0])
+	}
+
+	if root.Kind == yaml.DocumentNode {
+		if len(root.Content) > 0 {
+			return setValue(root.Content[0], data, keys...)
+		}
+
+		return appendDataToContent(root, data, keys...)
+	}
+
+	return fmt.Errorf("%w: key: %s", ErrUnexpectedNodeKind, keys[0])
 }
 
 func getValue(node *yaml.Node, keys ...string) (*yaml.Node, error) {
@@ -191,4 +231,42 @@ func isEmptyNode(node *yaml.Node) bool {
 		return len(node.Content) == 0
 	}
 	return false
+}
+
+// createTypedEnvelope recursively wraps the provided data into a nested structure
+// of maps or slices based on the provided restKeys.
+// If a key is "[]", it wraps the result in a slice.
+// Otherwise, it wraps the result in a map with the key as the property name.
+func createTypedEnvelope[DataType any](data DataType, restKeys ...string) any {
+	if len(restKeys) == 0 {
+		// Base case: no more keys to process, return the raw data
+		return data
+	}
+
+	if restKeys[0] == "[]" {
+		// List envelope: wrap the next level in a slice
+		return []any{createTypedEnvelope(data, restKeys[1:]...)}
+	}
+
+	// Map envelope: wrap the next level in a map using the current key
+	return map[string]any{restKeys[0]: createTypedEnvelope(data, restKeys[1:]...)}
+}
+
+// wrap any data in ContentNode to add/append to another Node
+func createContentNode[DataType any](data DataType) (*yaml.Node, error) {
+	node := yaml.Node{}
+	if err := node.Encode(data); err != nil {
+		return nil, fmt.Errorf("cannot encode value to yaml node: %w", err)
+	}
+
+	return &node, nil
+}
+
+func appendDataToContent[DataType any](node *yaml.Node, data DataType, keys ...string) error {
+	contentNode, err := createContentNode(createTypedEnvelope(data, keys...))
+	if err != nil {
+		return err
+	}
+	node.Content = append(node.Content, contentNode.Content...)
+	return nil
 }
